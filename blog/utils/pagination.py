ddbf8be20f8f30f8299469252190f4450301cd34
= <784078345@qq.com>
2019-01-23 12:18:08 +0800

    首页标题图片链接内容跳转

--- blog/utils/pagination.py
+++ blog/utils/pagination.py
@@ -5,6 +5,6 @@
 from rest_framework.pagination import PageNumberPagination
 
 class MyPageNumberPagination(PageNumberPagination):
-    page_size = 2  # 默认显示每页两条
+    page_size = 10  # 默认显示每页两条
     page_size_query_param = 'page'  # 查询关键字名称：第几页
     page_size_query_param='page_size'  # 查询关键字名称：每页多少条
